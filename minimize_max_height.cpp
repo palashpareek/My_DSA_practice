38883e0a163b06f8060a2ffa0201819d8a3b0357
Palash <palashpareek1998@gmail.com>
2021-10-18 17:58:41 +0530

    questions solved on 18 October 2021

---
+++ minimize_max_height.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        
+        sort(arr, arr+n);
+        
+        int ans = arr[n-1] - arr[0];
+        
+        int largest = arr[n-1]-k;
+        int smallest = arr[0]+k;
+        
+        int mi, ma;
+        
+        for(int i=0;i<n;i++)
+        {
+            ma = max(largest, arr[i]+k);
+            mi = min(smallest, arr[i+1]-k);
+            
+            if(mi<0)
+            {
+                continue;
+            }
+            
+            ans = min(ans, ma-mi);
+            
+        }
+        
+        return ans;
+        
+        
+        
+    }
+};
